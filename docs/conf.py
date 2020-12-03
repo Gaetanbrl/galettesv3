650e5c145793d96abddd434a937b353f20482079
gaetan <gaetan.bruel@jdev.fr>
2020-12-03 13:47:44 +0100

    more config to display tree

--- docs/conf.py
+++ docs/conf.py
@@ -68,8 +68,67 @@ language = 'fr'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+#'papersize': 'letterpaper',
+
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
+
+# Additional stuff for the LaTeX preamble.
+#'preamble': '',
+
+# Latex figure (float) alignment
+#'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'galette.tex', u'galette Documentation',
+     u'Communauté de la galette', 'manual'),
+]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+#latex_logo = None
+
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+#latex_show_urls = False
+
+# Documents to append as an appendix to all manuals.
+#latex_appendices = []
+
+# If false, no module index is generated.
+#latex_domain_indices = True
+
+
+# -- Options for manual page output ---------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'galette', u'galette Documentation',
+     [author], 1)
+]
+
+# If true, show URL addresses after external links.
+#man_show_urls = False
 
 # -- Options for HTML output -------------------------------------------------
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'galettepower'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
--- docs/index.rst
+++ docs/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to galettes's documentation!
+La galette bretonne
 ====================================
 
 .. toctree::
@@ -12,11 +12,3 @@ Welcome to galettes's documentation!
 
    doc_main/recette.rst
    doc_main/histoire.rst
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
